64f809b92590d75e2118572ba032d5f9babdadea
jobinzhang <619440047@qq.com>
2020-04-03 23:57:08 +0800

    底部栏

---
+++ latte-core/src/main/java/com/example/latte_core/delegates/bottom/BaseBottomDelegate.java
@@ -0,0 +1,24 @@
+package com.example.latte_core.delegates.bottom;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.latte_core.delegates.LatteDelegate;
+
+public class BaseBottomDelegate extends LatteDelegate {
+
+
+
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+    }
+}
